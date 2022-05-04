aaf860a926290512c355f632d6723aeeda89882a
Yunus Pektas <yunuspektas@protonmail.com>
2022-05-04 17:51:20 +0300

    adding task.txt

---
+++ src/LambdaRunner.java
@@ -0,0 +1,98 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LambdaRunner {
+    public static void main(String[] args) {
+        List<Integer> sayi = new ArrayList<>(Arrays.asList(34, 22, 16, 11, 35, 20, 63, 21, 65, 44, 66, 64, 81, 38, 15));
+
+        //Day-1
+
+        //Task : "Structured Programming" kullanarak list elemanlarını aynı satirda aralarında bosluk olacak sekilde print ediniz.
+        //Task : "functional Programming" kullanarak list elemanlarını aynı satirda aralarında bosluk olacak sekilde print ediniz.
+        //Task : functional Programming ile list elemanlarinin  cift olanalrini ayni satirda aralarina bosluk birakarak print ediniz.
+        //Task :functional Programming ile list elemanlarinin 34 den kucuk cift olanalrini ayni satirda aralarina bosluk birakarak print ediniz.
+        //Task : functional Programming ile list elemanlarinin 34 den buyk veya cift olanalrini ayni satirda aralarina bosluk birakarak print ediniz.
+
+        //Day-2
+
+        //   List<Integer> sayi = new ArrayList<>(Arrays.asList(34, 22, 16, 11, 35, 20, 63, 21, 65, 44, 66, 64, 81, 38, 15));
+
+        // Task : Functional Programming ile listin cift elemanlarinin  karelerini ayni satirda aralarina bosluk bırakarak print ediniz
+        // Task : Functional Programming ile listin tek elemanlarinin  kuplerinin bir fazlasini ayni satirda aralarina bosluk birakarak print edi
+        // Task : Functional Programming ile listin cift elemanlarinin   karekoklerini ayni satirda aralarina bosluk birakarak yazdiriniz
+        // Task : list'in en buyuk elemanini yazdiriniz
+        // Task : List'in cift elemanlarin karelerinin en buyugunu print ediniz
+        // Task : List'teki tum elemanlarin toplamini yazdiriniz.
+        // Task : List'teki cift elemanlarin carpimini  yazdiriniz.
+        // Task : List'teki elemanlardan en kucugunu 4 farklı yontem ile print ediniz.
+        // Task : List'teki 5'ten buyuk en kucuk tek sayiyi print ediniz.
+        // Task : list'in cift  elemanlarinin karelerini  kucukten buyuge print ediniz.
+        // Task : list'in tek  elemanlarinin kareleri ni buykten kucuge  print ediniz.
+
+        //Day-3
+
+
+        // List<String> menü = new ArrayList<>(Arrays.asList("küşleme", "adana", "trileçe", "havucDilim", "buryan",
+        //      "yaglama", "kokareç", "arabAşı", "güveç", "waffle", "welemen", "cacix"));
+
+
+        // Task : List elemanlarini alafabetik buyuk harf ve  tekrarsiz print ediniz.
+        // Task : list elelmanlarinin character sayisini ters sirali olarak tekrarsiz print ediniz..
+        // Task : List elemanlarini character sayisina gore kckten byk e gore print ediniz..
+        // Task : list elemanlarinin son harfine gore ters sirali print ediniz.
+        // Task : listin elemanlarin karakterlerinin cift sayili  karelerini hesaplayan,ve karelerini tekrarsiz buyukten kucuge sirali  print ediniz...
+        // Task : List elelmmalarinin karakter sayisini 7 ve 7 'den az olma durumunu kontrol ediniz.
+        // Task : List elelmanlarinin "W" ile baslamasını kontrol ediniz.
+        // Task : List elelmanlarinin "x" ile biten en az bir elemanı kontrol ediniz.
+        // Task : Karakter sayisi en buyuk elemani yazdiriniz.
+        // Task : list elemanlarini son harfine göre siralayıp ilk eleman hariç kalan elemanlari print ediniz.
+
+        //Day-4
+
+
+    /*
+    TASK :
+    fields --> Universite (String)
+               bolum (String)
+               ogrcSayisi (int)
+               notOrt (int)
+               olan POJO clas craete edip main method içinde 5 farklı obj'den List create ediniz.
+
+
+
+        Universite u01 = new Universite("bogazici", "matematik", 571, 93);
+        Universite u02 = new Universite("istanbul teknik", "matematik", 622, 81);
+        Universite u03 = new Universite("istanbul", "hukuk", 1453, 71);
+        Universite u04 = new Universite("marmara", "bilgisayar muh", 1071, 77);
+        Universite u05 = new Universite("yıldız teknik", "gemi", 333, 74);
+        List<Universite> unv = new ArrayList<>(Arrays.asList(u01, u02, u03, u04, u05));
+
+     */
+
+        //task 01--> notOrt'larinin 74' den buyuk oldg kontrol eden pr create ediniz.
+        //task 02-->ogrc sayilarinin   110 den az olmadigini  kontrol eden pr create ediniz.
+        //task 03-->universite'lerde herhangi birinde "matematik" olup olmadigini  kontrol eden pr create ediniz.
+        //task 04-->universite'leri ogr sayilarina gore b->k siralayiniz.
+        //task 05-->universite'leri notOrt gore  b->k siralayip ilk 3 'unu print ediniz.
+        //task 06--> ogrc sayisi en az olan 2. universite'yi  print ediniz.
+        //task 07--> notOrt 63 'den buyuk olan universite'lerin ogrc sayilarini toplamini print ediniz.
+        //task 08--> Ogrenci sayisi 333'dan buyuk olan universite'lerin notOrt'larinin ortalamasini bulunuz.
+        //task 09-->"matematik" bolumlerinin sayisini  print ediniz.
+        //task 10-->Ogrenci sayilari 571'dan fazla olan universite'lerin en buyuk notOrt'unu bulunuz.
+        //task 11-->Ogrenci sayilari 1071'dan az olan universite'lerin en kucuk notOrt'unu bulunuz.
+
+        //Day-5
+
+        //TASK 01 --> Structured Programming ve Functional Programming ile 1'den x'e kadar tamsayilari toplayan bir program create ediniz.
+        //TASK 02 --> 1'den x'e kadar cift tamsayilari toplayan bir program create ediniz.
+        //TASK 03 --> Ilk x pozitif cift sayiyi toplayan program  create ediniz.
+        //TASK 04 --> Ilk X pozitif tek tamsayiyi toplayan programi  create ediniz.
+        //TASK 05 --> 2'nin ilk x kuvvetini ekrana yazdiran programi  create ediniz.
+        //TASK 06 --> Istenilen bir sayinin ilk x kuvvetini ekrana yazdiran programi  create ediniz.
+        //TASK 07 --> Istenilen bir sayinin faktoriyelini hesaplayan programi  create ediniz.
+        //TASK 08 --> Istenilen bir sayinin  x. kuvvetini ekrana yazdiran programi  create ediniz.
+
+    }
+}
+
